c89fef792d30c933e35f8816a59d298480b1193b
Helen Oliveira <sousahelen816@gmail.com>
2025-03-03 23:38:40 -0300

    feat

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
+  nome: string;
   position: number;
-  weight: number;
-  symbol: string;
+  cargo: string;
+
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, nome: 'Hydrogen', cargo: 'Dev front end'},
+  {position: 2, nome: 'Helium', cargo: 'Dev back end'},
+  {position: 3, nome: 'Lithium', cargo: 'Dev front end'},
+  {position: 4, nome: 'Beryllium', cargo: 'Dev back end'},
+  {position: 5, nome: 'Boron', cargo: 'DevOps'},
+  {position: 6, nome: 'Carbon', cargo: 'Dev front end'},
+  {position: 7, nome: 'Nitrogen', cargo: 'DevOps'},
+  {position: 8, nome: 'Oxygen', cargo: 'Dev front end'},
+  {position: 9, nome: 'Fluorine', cargo: 'DevOps'},
+  {position: 10, nome: 'Neon', cargo: 'DevOps'},
 ];
 
 
@@ -32,6 +32,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class TabelaComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'nome', 'cargo'];
   dataSource = ELEMENT_DATA;
 }
